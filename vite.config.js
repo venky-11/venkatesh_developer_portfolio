bc72cc62dfbc884951f7e1409a40fc763049c972
Venkatesh kodgire <102906595+venky-11@users.noreply.github.com>
2024-09-19 13:48:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,19 @@
 //})
 
 
+//import { defineConfig } from 'vite';
+
+//export default defineConfig({
+ // base: '/<venkatesh_developer_portfolio>/',  // Replace <repo-name> with your actual repository name
+//});
+
+
+// vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/<venkatesh_developer_portfolio>/',  // Replace <repo-name> with your actual repository name
+  base: '/venkatesh_developer_portfolio/', // Ensure this matches your GitHub Pages URL
+  plugins: [react()],
 });
 
